dec21f4d813416256b488ead7ddc099f7dbba5e9
Elthon Urbina <uelthon95@gmail.com>
2022-10-08 01:09:45 -0600

    added sidebar and projects

--- script.js
+++ script.js
@@ -2,6 +2,37 @@ const menu = document.getElementById('menu')
 const navigate = document.getElementById('navigate')
 const closeMenu = document.getElementById('close-btn')  
 const link = document.querySelectorAll('#link')
+const containerProjects = document.querySelector('.project-items')
+const closeAside = document.getElementById('close-aside')
+const overlayAside = document.querySelector('.overlay-aside')
+const aside = document.querySelector('aside')
+const html = document.querySelector('html')
+const asideProject = document.querySelector('.aside-project')
+
+const projects = [
+  {
+    id: 1,
+    title: 'CellStore',
+    url: 'https://cellstore.onrender.com/',
+    path: 'assets/cellstore',
+    img: 'assets/cellstore/0.png',
+    backend: ['JavaScript', 'NodeJs', 'Express', 'MongoDB'],
+    frontend: ['ReactJS', 'React-Redux', 'React-Bootstrap'],
+    gallery: ['0','1','2','3','4'],
+    desc: 'An PWA E-commerce Application For Buying Smartphones.'
+  },
+  {
+    id: 2,
+    title: 'My Porfolio',
+    url: 'https://uelthon.github.io/porfolio/',
+    path: 'assets/myporfolio',
+    img: 'assets/myporfolio/0.png',
+    backend: [],
+    frontend: ['HTML5', 'CSS', 'JavaScript'],
+    gallery: ['0','1','2','3'],
+    desc: 'My Full Stack Web Developer Porfolio'
+  }
+]
 
 menu.addEventListener('click', (e) => {
   e.preventDefault()
@@ -17,4 +48,65 @@ link.forEach(e  => {
   e.addEventListener('click', () => {
     navigate.classList.remove('show-nav')
   })
+})
+
+function displayProject(id){
+  const item = projects.find(e => e.id === id)
+  if(!item) throw new Error({error: 'item dont exist'});
+  const displayItem = `
+                        <h2>${item.title}</h2>
+                        <p>${item.desc}</p>
+                        <div class="aside-project-gallery">
+                          ${item.gallery.map(e => `<img src=${item.path+'/'+e+'.png'} loading="lazy" />` ).join("")}
+                        </div>
+                        <a href=${item.url} target="_blank">Go to Project</a> 
+                      `
+   return asideProject.innerHTML = displayItem
+}
+
+function asideToggle(){
+  overlayAside.classList.toggle('show-overlay-aside')
+  aside.classList.toggle('show-aside')
+  html.classList.toggle('html-overflow-hidden')
+}
+
+closeAside.addEventListener('click',() => {
+  asideToggle()
+})
+overlayAside.addEventListener('click', () => {
+  asideToggle()
+})
+function preview(id){
+  try{
+    displayProject(id)
+    asideToggle()
+  }catch(e){
+    console.log(e)
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  let displayProjects = projects.map(e => {
+    return `
+          <div class="project-item">
+            <img class="project-img" src=${e.img} loading="lazy" />
+            <div class="project-info">
+              <h2>${e.title}</h2>
+              <p>${e.desc}</p>
+              <div class="project-tags">
+                ${e.frontend.map(e => `<div class="project-tag">${e}</div>`).join("")}
+              </div>
+              <div class="project-tags">
+                ${e.backend.map(e => `<div class="project-tag">${e}</div>`).join("")}
+              </div>
+              <div class='project-footer'>
+                <a href=${e.url} target="_blank" >Go to Project</a>
+                <button onclick="preview(${e.id})" >Previews</button>
+              </div>
+            </div>
+          </div>
+    `
+  })
+  displayProjects = displayProjects.join("")
+  containerProjects.innerHTML = displayProjects;
 })
